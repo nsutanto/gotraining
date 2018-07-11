1a9e4e9f6211dba2b71936403dbd01d7d33f13eb
Nicholas Sutanto <nsutanto@enova.com>
2018-07-11 09:31:50 -0500

    constant exercise

--- topics/go/language/constants/exercises/template1/template1.go
+++ topics/go/language/constants/exercises/template1/template1.go
@@ -6,20 +6,29 @@
 // Multiply two literal constants into a typed variable and display the value.
 package main
 
+import "fmt"
+
 // Add imports.
 
 const (
 // Declare a constant named server of kind string and assign a value.
+	server = "124.53.24.123"
+
 
 // Declare a constant named port of type integer and assign a value.
+	port int16 = 9000
 )
 
 func main() {
 
 	// Display the value of both server and port.
+	fmt.Println(server)
+	fmt.Println(port)
 
 	// Divide a constant of kind integer and kind floating point and
 	// assign the result to a variable.
+	minutes := 5320 / 60.0
 
 	// Display the value of the variable.
+	fmt.Println(minutes)
 }
