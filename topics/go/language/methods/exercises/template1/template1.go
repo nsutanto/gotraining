7e3a89b070f6913240b9e592108b6b58803e6e56
Nicholas Sutanto <nsutanto@enova.com>
2018-07-11 10:31:19 -0500

    methods exercise

--- topics/go/language/methods/exercises/template1/template1.go
+++ topics/go/language/methods/exercises/template1/template1.go
@@ -7,19 +7,43 @@
 // the slice displaying the players name and batting average.
 package main
 
+import "fmt"
+
+type player struct {
+	name string
+	atBats float64
+	hits float64
+}
 // Add imports.
 
 // Declare a struct that represents a ball player.
 // Include fields called name, atBats and hits.
 
 // Declare a method that calculates the batting average for a player.
-func ( /* receiver */ ) average() /* return type */ {
+func (p *player ) average() float64 {
+	if p.atBats == 0 {
+		return 0
+	}
+
+	return p.hits / p.atBats
 }
 
 func main() {
 
 	// Create a slice of players and populate each player
 	// with field values.
+	ps := []player{
+		{"bill", 10, 7},
+		{"jim", 12, 6},
+		{"ed", 6, 4},
+	}
 
 	// Display the batting average for each player in the slice.
+	for i := range ps {
+		fmt.Printf("%s: AVG[.%.f]\n", ps[i].name, ps[i].average()*1000)
+	}
+
+	for _, p := range ps {
+		fmt.Printf("%s: AVG[.%.f]\n", p.name, p.average()*1000)
+	}
 }
