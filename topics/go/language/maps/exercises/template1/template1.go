4c62b16f4dc00b5e16f7d00b8ec97a47c7774992
Nicholas Sutanto <nsutanto@enova.com>
2018-07-11 09:41:45 -0500

    map exercise

--- topics/go/language/maps/exercises/template1/template1.go
+++ topics/go/language/maps/exercises/template1/template1.go
@@ -5,13 +5,21 @@
 // map with five values and iterate over the map to display the key/value pairs.
 package main
 
-// Add imports.
+import "fmt"
 
 func main() {
 
 	// Declare and make a map of integer type values.
+	testMap := make(map[string]int)
 
 	// Initialize some data into the map.
+	testMap["Nicholas"] = 35
+	testMap["Joan"] = 34
 
 	// Display each key/value pair.
+	for key, value := range testMap {
+		fmt.Printf("Name: %s Age: %d\n", key, value)
+	}
+
+	fmt.Println(testMap)
 }
