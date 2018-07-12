86eeaf4d525f86c71dc32f57e2530510f0a9ea28
Nicholas Sutanto <nsutanto@enova.com>
2018-07-12 13:25:53 -0500

    exercise channels

--- topics/go/concurrency/channels/exercises/template2/template2.go
+++ topics/go/concurrency/channels/exercises/template2/template2.go
@@ -9,31 +9,52 @@
 // in a slice. Print the slice values then terminate the program.
 package main
 
+import (
+	"time"
+	"math/rand"
+	"fmt"
+)
+
 // Add imports.
 
 // Declare constant for number of goroutines.
+const (
+	goroutines = 100
+)
 
 func init() {
 	// Seed the random number generator.
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
 
 	// Create the buffered channel with room for
 	// each goroutine to be created.
+	values := make(chan int, goroutines)
 
 	// Iterate and launch each goroutine.
-	{
+	for gr := 0; gr < goroutines; gr++ {
 
 		// Create an anonymous function for each goroutine that
 		// generates a random number and sends it on the channel.
+		go func() {
+			values <- rand.Intn(50)
+		}()
 	}
 
 	// Create a variable to be used to track received messages.
 	// Set the value to the number of goroutines created.
+	wait := goroutines
 
 	// Iterate receiving each value until they are all received.
 	// Store them in a slice of ints.
+	var nums []int
+	for wait > 0 {
+		nums = append(nums, <-values)
+		wait--
+	}
 
 	// Print the values in our slice.
+	fmt.Println(nums)
 }
