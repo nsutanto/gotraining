12c8a5c55c7cd0a977780308f4f8deb3b0c32efb
Nicholas Sutanto <nsutanto@enova.com>
2018-07-12 09:49:35 -0500

    exercise goroutine

--- topics/go/concurrency/goroutines/exercises/template1/template1.go
+++ topics/go/concurrency/goroutines/exercises/template1/template1.go
@@ -10,35 +10,52 @@
 package main
 
 // Add imports.
-import "runtime"
+import (
+	"runtime"
+	"sync"
+	"fmt"
+)
 
 func init() {
 
 	// Allocate one logical processor for the scheduler to use.
-	runtime.GOMAXPROCS(1)
+	runtime.GOMAXPROCS(4)
 }
 
 func main() {
 
 	// Declare a wait group and set the count to two.
+	var wg sync.WaitGroup
+	wg.Add(2)
 
 	// Declare an anonymous function and create a goroutine.
-	{
+	go func() {
 		// Declare a loop that counts down from 100 to 0 and
 		// display each value.
+		for count := 100; count >= 0; count-- {
+			fmt.Printf("[A:%d]\n", count)
+		}
 
 		// Tell main we are done.
-	}
+		wg.Done()
+	}()
 
 	// Declare an anonymous function and create a goroutine.
-	{
+	go func() {
 		// Declare a loop that counts up from 0 to 100 and
 		// display each value.
+		for count := 0; count <= 100; count++ {
+			fmt.Printf("[B:%d]\n", count)
+		}
 
 		// Tell main we are done.
-	}
+		wg.Done()
+	}()
 
 	// Wait for the goroutines to finish.
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
 
 	// Display "Terminating Program".
+	fmt.Println("\nTerminating Program")
 }
